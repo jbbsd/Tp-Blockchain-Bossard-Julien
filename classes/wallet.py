6e2bfd2272aefbaeecc51895e8daa2cf608c39fe
jbbsd <julien4915@hotmail.com>
2021-07-09 15:54:14 +0200

    sub-balance fonctionnalité

--- classes/wallet.py
+++ classes/wallet.py
@@ -4,20 +4,55 @@ import json
 
 class Wallet(object):
     def __init__(self):
-        self.unique_id = uuid.uuid4()
-
-
+        self.balance = 0
+        self.history = []
+        self.generate_unique_id()
 
 
 
 def generate_unique_id():
+    unique_id = str(uuid.uuid4())
+    balance = 0
+    with open('content\wallets\''+unique_id+'.json','w') as f:
+        json.dump(unique_id,f,ensure_ascii=False, sort_keys=False,indent=4)
+    print(unique_id)
+    add_balance(unique_id,balance)
+
+
+
     
-    unique_id = uuid.uuid4()
-    wallet_id = json.dumps(unique_id.int)
-    with open('wallets','w') as f:
-        json.dump(wallet_id,f,indent=4, ensure_ascii=False, sort_keys=False)
+    
+def add_balance(unique_id,balance):
+        balance = int(input("Solde?"))
+        with open('content\wallets\''+unique_id+'.json','a') as f:
+            json.dump(balance,f, ensure_ascii=False, sort_keys=False,indent=6)
+        sub_balance(unique_id,balance)
 
 
-        
+def sub_balance(unique_id,balance):
+    question = input("Soustraire du solde? y/N")
+    if question == "y":
+        sub = int(input("Somme a soustraire?"))
+        balance = balance - sub 
+        print("Nouveau solde",balance)
+        with open('content\wallets\''+unique_id+'.json','a') as f:
+            json.dump(balance,f, ensure_ascii=False, sort_keys=False,indent=8)
+    elif question == "N":
+        print("Solde actuelle",balance)
+    else:
+        print("Veuillez rentrer une réponse correcte")
+        sub_balance(unique_id,balance)
+
+
+            
+
+
+
 generate_unique_id()
+
+
+
+
+
+
 
